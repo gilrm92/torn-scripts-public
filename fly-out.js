6c845f291e88ed26d8276698282a57838a7d5b65
Gilberto <gilbertorm.ramos@gmail.com>
2024-06-15 09:55:02 +0200

    Bumping version

--- fly-out.js
+++ fly-out.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fast-Flight Panel
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Creates a panel to help you fly out faster
 // @author       CrowleyJr
 // @match        www.torn.com/*
