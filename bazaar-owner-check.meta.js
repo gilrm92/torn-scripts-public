ee9467440a78cd330502089095416bf3264edafe
Gilberto <gilbertorm.ramos@gmail.com>
2024-06-18 08:47:10 +0200

    Adding bazaar-owner-check.js scripts

---
+++ bazaar-owner-check.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Torn Bazaar Owner Check
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Check status and revive of owner of the bazaar in the list of items.
+// @author       CrowleyJr[2827691]
+// @match        https://www.torn.com/*
+// @grant        none
+// ==/UserScript==
