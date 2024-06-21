a23498d380af3e903b971755f51b077cea8e0542
Gilberto <gilbertorm.ramos@gmail.com>
2024-06-21 10:32:31 +0200

    Adding poker mug scripts

---
+++ poker-mug.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Torn Poker Mugger
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Helps you to keep eye on your poker target
+// @author       CrowleyJr[2827691]
+// @match        https://www.torn.com/page.php?sid=holdem
+// @icon         https://img.icons8.com/?size=100&id=571&format=png&color=000000
+// @grant        GM_xmlhttpRequest
+// @connect      data.mongodb-api.com
+// @connect      api.torn.com
+// ==/UserScript==
