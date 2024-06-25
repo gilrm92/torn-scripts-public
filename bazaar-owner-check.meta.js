0c4a28f05c77f1a4827d655a3f2786796647a47d
Gilberto <gilbertorm.ramos@gmail.com>
2024-06-25 13:20:25 +0200

    Adding api counter

--- bazaar-owner-check.meta.js
+++ bazaar-owner-check.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Bazaar Owner Check
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Check status and revive of owner of the bazaar in the list of items.
 // @author       CrowleyJr[2827691]
 // @match        https://www.torn.com/*
