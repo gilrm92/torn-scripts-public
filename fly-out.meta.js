3e20f0ef67b01417b6bcd8ab8ef1b8587e48e12b
Gilberto <gilbertorm.ramos@gmail.com>
2024-07-03 16:29:43 +0200

    Allowing everyone to use

--- fly-out.meta.js
+++ fly-out.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fast-Flight Panel
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Creates a panel to help you fly out faster
 // @author       CrowleyJr
 // @match        www.torn.com/*
